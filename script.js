bb0c75c678ff43396108b45822bf403f7644763a
JayY4s <jay.smurf.rl.third@gmail.com>
2024-08-08 19:15:18 -0400

    Upgrade 0.1
    add button help,
    add button to place a marker with your cursor,
    change the display for the map,
    see on google street view the location,

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var map = L.map('map', { doubleClickZoom: false }).setView([0, 0], 2);
+$(document).ready(function () {
+    var map = L.map('map', { 
+        doubleClickZoom: false,
+        zoomControl: false
+    }).setView([0, 0], 2);
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: '© OpenStreetMap'
     }).addTo(map);
 
-    L.control.fullscreen().addTo(map);
+    L.control.zoom({ position: 'bottomright' }).addTo(map);
 
     var marker;
-    var savedIconUrl = localStorage.getItem('selectedMarkerIcon');
-    if (savedIconUrl === null || savedIconUrl === undefined) {
-        savedIconUrl = '0';
-    }
+    var currentLatLng = { lat: 0, lng: 0 };
+    var savedIconUrl = localStorage.getItem('selectedMarkerIcon') || '0';
 
-    document.getElementById('markerSelect').value = savedIconUrl;
+    $('#markerSelect').val(savedIconUrl);
     changeMarker(); 
 
-    function changeMarker() {
-        var selectedMarker = document.getElementById('markerSelect').value;
+    function getCustomIcon(selectedMarker) {
         var markerUrl;
         var shadowUrl;
-        var customIcon;
 
         switch (selectedMarker) {
             case '0':
                 markerUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png';
                 shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
-                customIcon = L.icon({
+                return L.icon({
                     iconUrl: markerUrl,
                     shadowUrl: shadowUrl,
                     iconSize: [25, 41],
@@ -35,101 +35,103 @@ document.addEventListener("DOMContentLoaded", function () {
                     shadowSize: [41, 41],
                     shadowAnchor: [12, 41]
                 });
-                break;
-            case '1':
-                markerUrl = 'https://leafletjs.com/examples/custom-icons/leaf-green.png';
-                shadowUrl = 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png';
-                customIcon = L.icon({
-                    iconUrl: markerUrl,
-                    shadowUrl: shadowUrl,
-                    iconSize: [38, 95],
-                    iconAnchor: [22, 94],
-                    popupAnchor: [-3, -76],
-                    shadowSize: [50, 64],
-                    shadowAnchor: [4, 62]
-                });
-                break;
-            case '2':
-                markerUrl = 'https://leafletjs.com/examples/custom-icons/leaf-red.png';
-                shadowUrl = 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png';
-                customIcon = L.icon({
-                    iconUrl: markerUrl,
-                    shadowUrl: shadowUrl,
-                    iconSize: [38, 95],
-                    iconAnchor: [22, 94],
-                    popupAnchor: [-3, -76],
-                    shadowSize: [50, 64],
-                    shadowAnchor: [4, 62]
-                });
-                break;
-            case '3':
-                markerUrl = 'https://leafletjs.com/examples/custom-icons/leaf-orange.png';
-                shadowUrl = 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png';
-                customIcon = L.icon({
-                    iconUrl: markerUrl,
-                    shadowUrl: shadowUrl,
-                    iconSize: [38, 95],
-                    iconAnchor: [22, 94],
-                    popupAnchor: [-3, -76],
-                    shadowSize: [50, 64],
-                    shadowAnchor: [4, 62]
-                });
-                break;
-            case '4':
-                markerUrl = 'images/icon.png';
-                shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
-                customIcon = L.icon({
-                    iconUrl: markerUrl,
-                    shadowUrl: shadowUrl,
-                    iconSize: [50, 50],
-                    iconAnchor: [25, 50],
-                    popupAnchor: [0, -50],
-                    shadowSize: [50, 50],
-                    shadowAnchor: [15, 55]
-                });
-                break;
-            case '5':
-                markerUrl = 'images/brazil-pin.png';
-                shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
-                customIcon = L.icon({
-                    iconUrl: markerUrl,
-                    shadowUrl: shadowUrl,
-                    iconSize: [50, 50],
-                    iconAnchor: [25, 50],
-                    popupAnchor: [0, -50],
-                    shadowSize: [50, 50],
-                    shadowAnchor: [15, 55]
-                });
-                break;
-            case '6':
-                markerUrl = 'images/penguin.png';
-                shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
-                customIcon = L.icon({
-                    iconUrl: markerUrl,
-                    shadowUrl: shadowUrl,
-                    iconSize: [45, 45],
-                    iconAnchor: [20, 45],
-                    popupAnchor: [0, -45],
-                    shadowSize: [45, 45],
-                    shadowAnchor: [10, 50]
-                });
-                break;
-            default:
-                markerUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png';
-                shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
-                customIcon = L.icon({
-                    iconUrl: markerUrl,
-                    shadowUrl: shadowUrl,
-                    iconSize: [25, 41],
-                    iconAnchor: [12, 41],
-                    popupAnchor: [1, -34],
-                    shadowSize: [41, 41],
-                    shadowAnchor: [12, 41]
-                });
-                break;
+                case '1':
+                    markerUrl = 'https://leafletjs.com/examples/custom-icons/leaf-green.png';
+                    shadowUrl = 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png';
+                    return L.icon({
+                        iconUrl: markerUrl,
+                        shadowUrl: shadowUrl,
+                        iconSize: [38, 95],
+                        iconAnchor: [22, 94],
+                        popupAnchor: [-3, -76],
+                        shadowSize: [50, 64],
+                        shadowAnchor: [4, 62]
+                    });
+                case '2':
+                    markerUrl = 'https://leafletjs.com/examples/custom-icons/leaf-red.png';
+                    shadowUrl = 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png';
+                    return L.icon({
+                        iconUrl: markerUrl,
+                        shadowUrl: shadowUrl,
+                        iconSize: [38, 95],
+                        iconAnchor: [22, 94],
+                        popupAnchor: [-3, -76],
+                        shadowSize: [50, 64],
+                        shadowAnchor: [4, 62]
+                    });
+                case '3':
+                    markerUrl = 'https://leafletjs.com/examples/custom-icons/leaf-orange.png';
+                    shadowUrl = 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png';
+                    return L.icon({
+                        iconUrl: markerUrl,
+                        shadowUrl: shadowUrl,
+                        iconSize: [38, 95],
+                        iconAnchor: [22, 94],
+                        popupAnchor: [-3, -76],
+                        shadowSize: [50, 64],
+                        shadowAnchor: [4, 62]
+                    });
+                case '4':
+                    markerUrl = 'images/icon.png';
+                    shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
+                    return L.icon({
+                        iconUrl: markerUrl,
+                        shadowUrl: shadowUrl,
+                        iconSize: [50, 50],
+                        iconAnchor: [25, 50],
+                        popupAnchor: [0, -50],
+                        shadowSize: [50, 50],
+                        shadowAnchor: [15, 55]
+                    });
+                case '5':
+                    markerUrl = 'images/brazil-pin.png';
+                    shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
+                    return L.icon({
+                        iconUrl: markerUrl,
+                        shadowUrl: shadowUrl,
+                        iconSize: [50, 50],
+                        iconAnchor: [25, 50],
+                        popupAnchor: [0, -50],
+                        shadowSize: [50, 50],
+                        shadowAnchor: [15, 55]
+                    });
+                case '6':
+                    markerUrl = 'images/penguin.png';
+                    shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
+                    return L.icon({
+                        iconUrl: markerUrl,
+                        shadowUrl: shadowUrl,
+                        iconSize: [45, 45],
+                        iconAnchor: [20, 45],
+                        popupAnchor: [0, -45],
+                        shadowSize: [45, 45],
+                        shadowAnchor: [10, 50]
+                    });
+                default:
+                    markerUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png';
+                    shadowUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png';
+                    return L.icon({
+                        iconUrl: markerUrl,
+                        shadowUrl: shadowUrl,
+                        iconSize: [25, 41],
+                        iconAnchor: [12, 41],
+                        popupAnchor: [1, -34],
+                        shadowSize: [41, 41],
+                        shadowAnchor: [12, 41]
+                    });
         }
+    }
 
-        // Remove previous marker if exists
+    function openStreetView(lat, lng) {
+        var googleStreetViewUrl = `https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=${lat},${lng}`;
+        window.open(googleStreetViewUrl, '_blank');
+    }
+
+    function changeMarker() {
+        var selectedMarker = $('#markerSelect').val();
+        var customIcon = getCustomIcon(selectedMarker);
+
+        // Remove previous marker if it exists
         if (marker) {
             map.removeLayer(marker);
         }
@@ -138,24 +140,36 @@ document.addEventListener("DOMContentLoaded", function () {
             .bindPopup(`<b>Coordinates:</b> ${0.000000.toFixed(6)}, ${0.000000.toFixed(6)}`)
             .openPopup();
 
-        document.getElementById('selectedMarkerImage').src = markerUrl;
-        // Save selected marker icon URL to localStorage
+        setInterval(function () {
+            if (!marker.getPopup().isOpen()) {
+                marker.openPopup();
+            }
+        }, 1000);
+        
+        marker.on('click', function(e){
+            var lat = e.latlng.lat;
+            var lng = e.latlng.lng;
+            openStreetView(lat, lng);
+        });
+
+        $('#selectedMarkerImage').attr('src', getCustomIcon(selectedMarker).options.iconUrl);
         localStorage.setItem('selectedMarkerIcon', selectedMarker);
     }
 
-    document.getElementById('markerSelect').addEventListener('change', function () {
+    $('#markerSelect').on('change', function () {
         changeMarker();
     });
 
-    function updateMarker(lat, lon) {
+    function updateMarker(lat, lon, zoom = map.getZoom()) {
+        currentLatLng = { lat: lat, lng: lon };
         var newLatLng = new L.LatLng(lat, lon);
         marker.setLatLng(newLatLng);
         marker.getPopup().setContent(`<b>Coordinates:</b> ${lat.toFixed(6)}, ${lon.toFixed(6)}`).update();
-        map.setView(newLatLng, map.getZoom());
+        map.setView(newLatLng, zoom);
     }
 
     window.updateCoordinates = function () {
-        var coordinates = document.getElementById('coordinates').value;
+        var coordinates = $('#coordinates').val();
         if (coordinates) {
             var parts = coordinates.split(',');
 
@@ -184,10 +198,9 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     function showAlert(message) {
-        var alertMessage = document.getElementById('alertMessage');
-        alertMessage.innerHTML = message;
+        $('#alertMessage').html(message);
 
-        var myModal = new bootstrap.Modal(document.getElementById('alertBox'), {
+        var myModal = new bootstrap.Modal($('#alertBox')[0], {
             backdrop: 'static',
             keyboard: false,
             focus: false
@@ -195,16 +208,16 @@ document.addEventListener("DOMContentLoaded", function () {
         myModal.show();
     }
 
-    window.reset = function () {
-        document.getElementById('coordinates').value = '';
-        document.getElementById('latitude').value = '';
-        document.getElementById('longitude').value = '';
-        updateMarker(0, 0);
+    window.resetMarker = function () {
+        $('#coordinates').val('');
+        $('#latitude').val('');
+        $('#longitude').val('');
+        updateMarker(0, 0, 2);
     };
 
     window.updateLatLon = function () {
-        var lat = parseFloat(document.getElementById('latitude').value);
-        var lon = parseFloat(document.getElementById('longitude').value);
+        var lat = parseFloat($('#latitude').val());
+        var lon = parseFloat($('#longitude').val());
 
         if (isNaN(lat) || isNaN(lon)) {
             showAlert('Latitude and Longitude must be numbers.');
@@ -234,21 +247,66 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     function showSetting() {
-        var settingWindow = document.getElementById('setting-window');
-        var overlay = document.getElementById('overlay');
-
-        settingWindow.style.display = 'block';
-        overlay.style.display = 'block';
+        $('#setting-window').show();
+        $('#overlay').show();
     }
 
     function hideSetting() {
-        var settingWindow = document.getElementById('setting-window');
-        var overlay = document.getElementById('overlay');
-
-        settingWindow.style.display = 'none';
-        overlay.style.display = 'none';
+        $('#setting-window').hide();
+        $('#overlay').hide();
     }
 
-    document.getElementById('setting-button').addEventListener('click', showSetting);
-    document.getElementById('close-setting').addEventListener('click', hideSetting);
+    function showHelp() {
+        $('#help-window').show();
+        $('#overlay').show();
+    }
+
+    function hideHelp() {
+        $('#help-window').hide();
+        $('#overlay').hide();
+    }
+
+    $('#setting-button').on('click', showSetting);
+    $('#close-setting').on('click', hideSetting);
+    $('#help-button').on('click', showHelp);
+    $('#close-help').on('click', hideHelp);
+
+    // place marker button
+    var message = $('#message');
+
+    $('#place-marker-button').on('click', function() {
+        // Remove the current marker if it exists
+        if (marker) {
+            map.removeLayer(marker);
+        }
+
+        map.getContainer().style.cursor = 'crosshair';
+
+        message.text('Click on the map to place a marker')
+        // Listen for the map click event
+        map.once('click', function(e) {
+            // Get the coordinates
+            const lat = e.latlng.lat;
+            const lng = e.latlng.lng;
+    
+            // Place the new marker at the clicked location
+            const icon = getCustomIcon($('#markerSelect').val());
+            marker = L.marker(e.latlng, { icon: icon }).addTo(map);
+    
+            // Add a popup to the marker
+            marker.bindPopup(`<b>Coordinates:</b> ${lat.toFixed(6)}, ${lng.toFixed(6)}`)
+                .openPopup();
+    
+            // Attach the Street View click event
+            marker.on('click', function() {
+                openStreetView(lat, lng);
+            });
+    
+            // Reset cursor to default after placing the marker
+            map.getContainer().style.cursor = '';
+    
+            // Clear the message after placing the marker
+            message.text('')
+        });
+    });
 });
